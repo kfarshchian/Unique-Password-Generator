72a04ce280502264e79c46b7751cc45c52ef7cce
Kameron <kfarshchian@gmail.com>
2022-11-04 12:13:56 -0600

    added functions and promts for yes/no character types. Double check them all stayed up until 4am so may have many errors.

--- script.js
+++ script.js
@@ -4,26 +4,62 @@ const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","
 const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const specialCharacter = ["	U+0021","U+0022","U+0023","U+0024","U+0025","U+0026","U+0027","U+0028","U+0029","U+002A","U+002F","U+005C","U+003C","U+003E",];
 
- const passwordLength = function (){
-    passwordLength = window.prompt("Enter the length you would like the password. (Minimum of 8 maximum of 128 characters)")
-    if (passwordLength >= 8 && passwordLength <= 128){
+function createPasswordLength() {
+    var passwordLength = prompt("How many characters is your password? (Minimum 8 Maximum 128)");
+    while (passwordLength < 8 || passwordLength > 128 || passwordLength == "") {
+      alert("Error: Password length does not meet specifications.")
+      passwordLength = prompt("How many characters is your password? (Minimum 8 Maximum 128)");
     }
-    else{
-      window.alert("Invalid length, try again.")
-    }
- }
+    return passwordLength;
+  };
+  
+  function passwordCharacters() {
+    alert("Thank you for using Kameron's Unique password generator. What types of characters does your password require?");
+    var forLowercase = prompt("Do you require lowercase?");
+    var forUppercase = prompt("Do you require uppercase?");
+    var forNumbers = prompt("Do you require numbers?");
+    var forCharacters = prompt("Do you require special characters?");
+    var confirmArray = new Array(forLowercase, forUppercase, forNumbers, forCharacters);
+    var allPromptsFalseCheck = confirmArray.every( confirmElement  => !confirmElement );
+    if (allPromptsFalseCheck) {
+       alert("Invalid response. What types of characters does your password require?")
+       passwordCharacters();   
+    };
+    var passwordCharacters = '';
 
-// Get references to the #generate element
+  if (forLowercase == "Y", "yes") { 
+     passwordCharacters += lowerCase; 
+  }
+  if (forUppercase == "Y", "yes") { 
+     passwordCharacters += upperCase; 
+  }
+  if (forNumbers == "Y", "yes") { 
+     passwordCharacters += number; 
+  }
+  if (forCharacters == "Y", "yes") { 
+     passwordCharacters += specialCharacter; 
+  }
+
+  return passwordCharacters;
+};
+
+function generatePassword() {
+  var userLength = createPasswordLength();
+  var userCharacters = passwordCharacters();
+
+  var userPassword = "";
+  for (let i = 0; i < userLength; i++) {
+    userPassword += userCharacters.charAt(Math.floor(Math.random() * userCharacters.length));
+  }
+  return userPassword;
+};
+
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
