43d688c4b175d85c0d1fd6d02cf49a4bbabad78e
Kameron <kfarshchian@gmail.com>
2022-11-04 01:51:03 -0600

    change vars to const. Add passwordlength function

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 // Assignment code here
-var number = ["0","1","2","3","4","5","6","7","8","9"];
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var specialCharacter = ["	U+0021","U+0022","U+0023","U+0024","U+0025","U+0026","U+0027","U+0028","U+0029","U+002A","U+002F","U+005C","U+003C","U+003E",];
+const number = ["0","1","2","3","4","5","6","7","8","9"];
+const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const specialCharacter = ["	U+0021","U+0022","U+0023","U+0024","U+0025","U+0026","U+0027","U+0028","U+0029","U+002A","U+002F","U+005C","U+003C","U+003E",];
+
+ const passwordLength = function (){
+    passwordLength = window.prompt("Enter the length you would like the password. (Minimum of 8 maximum of 128 characters)")
+    if (passwordLength >= 8 && passwordLength <= 128){
+    }
+    else{
+      window.alert("Invalid length, try again.")
+    }
+ }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
